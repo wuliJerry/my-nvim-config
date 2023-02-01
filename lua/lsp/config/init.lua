9cd1d2b890799985b04a74d991a7185661b55715
wuliJerry <wuliJerry927@outlook.com>
2023-02-01 23:11:00 +0800

    Add lsp configuration for css and html

--- lua/lsp/config/init.lua
+++ lua/lsp/config/init.lua
@@ -5,4 +5,6 @@ require'lspconfig'.marksman.setup{}
 require'lspconfig'.pyright.setup{}
 require'lspconfig'.svls.setup{}
 require'lspconfig'.vtsls.setup{}
+require'lspconfig'.html.setup{}
+require'lspconfig'.cssls.setup{}
 
